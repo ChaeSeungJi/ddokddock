c63623dead5daa218237ff425954c82ee0cf97e0
채승지 <cotmdwl1405@gmail.com>
2024-05-28 00:46:46 +0900

    feat : session 추가

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 var createError = require("http-errors");
 var express = require("express");
+var session = require("express-session");
+var MemoryStore = require("memorystore")(session);
+var bodyParser = require("body-parser");
 var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
@@ -10,6 +13,7 @@ var loginRouter = require("./routes/login");
 var searchRouter = require("./routes/search");
 var tagRouter = require("./routes/tag");
 var listRouter = require("./routes/list");
+var studyRouter = require("./routes/study");
 
 var app = express();
 
@@ -17,9 +21,22 @@ var app = express();
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
+// 세션 설정
+app.use(
+  session({
+    secret: "your secret key", // 세션을 암호화하기 위한 비밀키
+    resave: false, // 세션을 항상 저장할지 결정 (false 권장)
+    saveUninitialized: true, // 초기화되지 않은 세션을 저장소에 저장할지 결정
+    store: new MemoryStore({ checkPeriod: 60 * 1000 }),
+    cookie: {
+      maxAge: 1000 * 60 * 60 * 24, // 쿠키 유효기간 (예: 1일)
+    },
+  })
+);
+
 app.use(logger("dev"));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
@@ -27,17 +44,18 @@ app.use("/", indexRouter);
 app.use("/users", usersRouter);
 app.use("/login", loginRouter);
 app.use("/search", searchRouter);
-app.use("/tag", tagRouter); // localhost:3000/tag 
+app.use("/tag", tagRouter); // localhost:3000/tag
 app.use("/list", listRouter);
+app.use("/study", studyRouter);
 
 app.use(function (req, res, next) {
-  console.log('404 Error: ', req.url);
+  console.log("404 Error: ", req.url);
   next(createError(404));
 });
 
 // error handler
 app.use(function (err, req, res, next) {
-  console.error('Error handler: ', err.stack);
+  console.error("Error handler: ", err.stack);
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get("env") === "development" ? err : {};
@@ -47,6 +65,6 @@ app.use(function (err, req, res, next) {
   res.render("error");
 });
 
-console.log('Server started');
+console.log("Server started");
 
 module.exports = app;
