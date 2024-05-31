b2aa972cc2429c45f558c8329c01d91362f0cf6c
yunseo <abab23490@gmail.com>
2024-06-01 00:29:34 +0900

    feat: 페이징 처리로 수정
    목록 전체 보기 페이징 처리로 수정

--- routes/list.js
+++ routes/list.js
@@ -44,33 +44,3 @@ router.get('/', (req, res) => {
 
   module.exports = router;
   
-/*const express = require('express');
-const router = express.Router();
-const bodyParser = require('body-parser');
-const db = require('../util/db');
-
-router.use(bodyParser.urlencoded({ extended: true }));
-
-router.get('/', (req, res) => {
-  const query = `
-    SELECT s.study_id, s.title, SUBSTR(s.content, 1, 20) AS content, s.image_url, m.nickname AS leader_nickname, s.created_at
-    FROM study s
-    JOIN member m ON s.leader_id = m.member_id
-    ORDER BY s.created_at DESC
-  `;
-  
-  db.query(query, (error, results) => {
-    if (error) {
-      console.error('스터디 목록 조회 중 오류 발생: ', error);
-      res.status(500).json({ error: '스터디 목록 조회 중 오류가 발생했습니다.' });
-    } else {
-      if (results.length === 0) {
-        res.status(404).json({ message: '스터디 목록이 비어 있습니다.' });
-      } else {
-        res.json(results);
-      }
-    }
-  });
-});
-
-module.exports = router;*/
