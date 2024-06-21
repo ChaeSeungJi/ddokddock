e0eb1846c4f4ada469b07aa616d275bb4900d9d8
yunseo <abab23490@gmail.com>
2024-06-21 15:15:43 +0900

    feat: post로 수정
    get, post로 수정

--- routes/study/tag.js
+++ routes/study/tag.js
@@ -5,7 +5,7 @@ const db = require('../../util/db');
 
 router.use(express.urlencoded({ extended: true }));
 
-router.get('/', (req, res) => {
+router.post('/', (req, res) => {
   const { keyword, sort, page, perPage } = req.query;
 
   if (!page || !perPage) {
